21f0b8eb1b16416daac58d6780e36d59121b5121
moratorium08 <moratorium08@gmail.com>
2018-01-01 01:08:02 +0900

    fix to use only mem

--- nenga17/subleq.py
+++ nenga17/subleq.py
@@ -1,7 +1,6 @@
 import sys
 mem = []
 pc = 0
-src = []
 
 def get_val(addr):
     # input
@@ -16,21 +15,25 @@ def set_val(addr, val):
         return sys.stdout.write(chr(val))
     while len(mem) <= addr:
         mem.append(0)
-    mem[addr] = val & 0xff
+    mem[addr] = val
 
 def mainloop(src):
-    while pc < len(src):
-        r1, r2, addr = src[pc]
+    while pc + 2 < len(mem):
+        r1, r2, addr = mem[pc], mem[pc + 1], mem[pc + 2]
         val =  get_val(r2) - get_val(r1)
         set_val(r1, val)
         if val == 0:
             pc = addr
         else:
-            pc += 1
+            pc += 3
 
 def main():
-    ls = input().split(" ")
-    while len(ls) == 3:
-        src.append([int(x, 16) for x in ls])
-        ls = input().split(" ")
-    mainloop(src)
+    while True:
+        tmp = ""
+        while True:
+            x = sys.stdin.read(1)
+            if x in ' \n\t':
+                tmp += x
+                mem.append(int(tmp, 16))
+                tmp = ""
+    mainloop(mem)
